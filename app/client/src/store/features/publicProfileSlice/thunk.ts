ca3696b584e6e2a6bd21aafba768a88b961c0347
marko pajkic <markopajkic@ymail.com>
2023-03-09 10:59:28 +0100

    prode fix

--- app/client/src/store/features/publicProfileSlice/thunk.ts
+++ app/client/src/store/features/publicProfileSlice/thunk.ts
@@ -4,7 +4,8 @@ import http from 'http/api'
 
 export const getPublicProfile = createAsyncThunk('publicProfile/getUser', async (id: number, { rejectWithValue }) => {
   try {
-    console.log('getPublicProfile thunk', id, typeof id) // stays for testing in prod
+    // console.log('getPublicProfile thunk', id, typeof id) // stays for testing in prod
+    console.log('getPublicProfile thunk response', await http.auth.getPublicUser(id)) // stays for testing in prod
     return await http.auth.getPublicUser(id)
   } catch (error) {
     if (error instanceof Error) {
