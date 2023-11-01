994832d7876909fe1a5c4c55ef9286d65e3afc6c
markopajkic <marko.pajkic@jobstep.com>
2023-11-01 19:41:22 +0100

    pg connection change

--- app/server/src/modules/prisma/prisma.service.ts
+++ app/server/src/modules/prisma/prisma.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { ConfigurationService } from '../configuration/configuration.service';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor(confService: ConfigurationService) {
-    super({
-      datasources: {
-        db: {
-          url: confService.databaseUrl,
-        },
-      },
-    });
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
   }
 }
+
+// @Injectable()
+// export class PrismaService extends PrismaClient {
+//   constructor(confService: ConfigurationService) {
+//     super({
+//       datasources: {
+//         db: {
+//           url: confService.databaseUrl,
+//         },
+//       },
+//     });
+//   }
+// }
