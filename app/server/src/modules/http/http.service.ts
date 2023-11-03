1d43c6d16a66764e6fe939d8ac6481de9ea0af1c
markopajkic <marko.pajkic@jobstep.com>
2023-11-03 10:06:02 +0100

    config service and cors service refactored

--- app/server/src/modules/http/http.service.ts
+++ app/server/src/modules/http/http.service.ts
@@ -3,38 +3,13 @@ import { ConfigurationService } from '../configuration/configuration.service';
 
 @Injectable()
 export class HttpService {
-  env: string;
-
-  private serverLookupTable: { [key: string]: string } = {};
-  private clientLookupTable: { [key: string]: string } = {};
-
-  constructor(private configurationService: ConfigurationService) {
-    const env = this.configurationService.nodeEnvironment;
-
-    const serverLookupTable = {
-      development: this.configurationService.baseUrlServer,
-      test: '',
-      production: '',
-    };
-
-    const clientLookupTable = {
-      development: this.configurationService.baseUrlClient,
-      test: '',
-      production: '',
-    };
-
-    this.env = env;
-    this.serverLookupTable = serverLookupTable;
-    this.clientLookupTable = clientLookupTable;
-  }
+  constructor(private configurationService: ConfigurationService) {}
 
   baseUrlServer(sufix: string) {
-    // return `${this.serverLookupTable[this.env]}${sufix}`;
     return `${this.configurationService.baseUrlServer}${sufix}`;
   }
 
   baseUrlClient(sufix: string) {
-    // return `${this.clientLookupTable[this.env]}${sufix}`;
     return `${this.configurationService.baseUrlClient}${sufix}`;
   }
 }
