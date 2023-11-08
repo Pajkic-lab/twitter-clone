a3ccf649c16e47cba6f40a54ece22ab1471b3fac
markopajkic <marko.pajkic@jobstep.com>
2023-11-08 16:05:40 +0100

    wip app works, to do move prisma to separte lib

--- app/client/src/GlobalStyle.ts
+++ app/client/src/GlobalStyle.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
-import chipRegular from 'fonts/chip-regular.woff'
-import chipBold from 'fonts/chip-bold.woff'
+import chipRegular from './fonts/chip-regular.woff'
+import chipBold from './fonts/chip-bold.woff'
 import { Colors } from 'ui/styles'
 
 export const GlobalStyle = createGlobalStyle<{ theme: any; customCss: string }>`
