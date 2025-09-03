d7b91ec64f29b02d3e6002c96a663b0442e11e12
marko pajkic <markopajkic@ymail.com>
2025-09-03 15:25:50 +0200

    test

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,9 @@
-import { BreakingPointSetter } from "@tw/ui/common";
-import { ModalBackground } from "@tw/ui/components";
-import { ModalProvider } from "styled-react-modal";
-import { GlobalStyle } from "./GlobalStyle";
-import { Router } from "./router";
+import { BreakingPointSetter } from '@tw/ui/common';
+import { ModalBackground } from '@tw/ui/components';
+import { ModalProvider } from 'styled-react-modal';
+import { GlobalStyle } from './GlobalStyle';
+import { Router } from './router';
 
-//
 export const App = () => {
   return (
     <>
